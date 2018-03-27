a8f2d044b5390e0103c8eee59533d4131b277ade
RachelBegbie <35932225+RachelBegbie@users.noreply.github.com>
2018-03-27 17:03:03 +0100

    Add files via upload

---
+++ summykataexercise.js
@@ -0,0 +1,18 @@
+/*Instructions: Write a function that takes a string which has integers inside 
+it separated by spaces, and your task is to convert each integer 
+in the string into an integer and return their sum.
+*/
+
+let stringTwo = "1 2 3 4";
+
+function testTwo(string){
+let array = string.split(' ');
+let sumInt = 0;
+for(let i = 0; i<array.length; i++){
+    sumInt += array[i];
+}
+return sumInt; 
+    } 
+console.log(testTwo(stringTwo));
+
+//so my issue here is that it logs out 01234, which means the array is an array of strings of numbers ie ['1', '2', '3', '4'] instead of [1, 2, 3, 4]//
