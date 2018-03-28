6afed17bfb3afa6a135ae9efbfcc0d5978a096be
Rachel Begbie <rachel.k.begbie@gmail.com>
2018-03-28 10:10:06 +0100

    formatted code

--- summykataexercise.js
+++ summykataexercise.js
@@ -5,14 +5,14 @@ in the string into an integer and return their sum.
 
 let stringTwo = "1 2 3 4";
 
-function testTwo(string){
-let array = string.split(' ');
-let sumInt = 0;
-for(let i = 0; i<array.length; i++){
-    sumInt += parseInt(array[i]);
+function testTwo(string) {
+    let array = string.split(' ');
+    let sumInt = 0;
+    for (let i = 0; i < array.length; i++) {
+        sumInt += parseInt(array[i]);
+    }
+    return sumInt;
 }
-return sumInt; 
-    } 
 console.log(testTwo(stringTwo));
 
 //so my issue here is that it logs out 01234, which means the array is an array of strings of numbers ie ['1', '2', '3', '4'] instead of [1, 2, 3, 4]//
